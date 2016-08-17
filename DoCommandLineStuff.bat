40fdfc80287320b1ac4bca835033e0e9e71c84c1
Jezz Santos <jezz.santos@hotmail.com>
2016-08-17 12:21:26 +1200

    Created repo

---
+++ DoCommandLineStuff.bat
@@ -0,0 +1,50 @@
+ECHO ON
+CD /d %~dp0
+
+SET ipaddress=%1
+SET certpath=%2
+SET silent=%3
+IF "%certpath%"=="" (
+	SET certpath=Certificates
+)
+IF "%ipaddress%"=="" (
+	ECHO:
+	CALL SetColor.bat 0F "---- You must provide an ipaddress as first argument!"
+	ECHO(
+	ECHO:
+	PAUSE
+	goto :EOF
+)
+
+
+REM Check if the batch file is running elevated
+net session >nul 2>&1
+IF not %errorlevel% equ 0 (
+	ECHO:
+	CALL SetColor.bat 0F "---- You must run this batch file as administrator!"
+	ECHO(
+	ECHO:
+	PAUSE
+	goto :EOF
+)
+
+REM Setup the Visual Studio Build Environment
+CALL "%vs120comntools%\VsDevCmd.bat"
+IF %errorlevel% neq 0 GOTO :error
+
+ECHO:
+CALL SetColor.bat 0A "---- All Good it worked OK!"
+ECHO(
+ECHO:
+
+IF NOT "%silent%"=="q" PAUSE
+EXIT /b 0
+
+:error
+CD /d %~dp0
+ECHO:
+ECHO:
+CALL SetColor.bat 04 "---- Failed Install! error #%errorlevel% ----"
+ECHO(
+PAUSE
+EXIT /b %errorlevel%
---
+++ UnitTestProject1/IntegrationTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [AssemblyInitialize]
+        public static void InitialiseAllTests(TestContext content)
+        {
+            //Run once at start of the test run, before all tests
+            RunBatchCommands();
+        }
+
+        private static void RunBatchCommands()
+        {
+            // Look in the solution for this batch file
+            var batchFilePath =
+                Environment.ExpandEnvironmentVariables(Environment.CurrentDirectory +
+                                                       @"\..\..\..\DoCommandLineStuff.bat");
+            var startInfo = new ProcessStartInfo(batchFilePath, @"anip acertpath q")
+            {
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                RedirectStandardOutput = false,
+                RedirectStandardInput = false,
+                Verb = @"runas" // needs to run elevated
+            };
+
+            using (var process = new Process {StartInfo = startInfo})
+            {
+                process.Start();
+                process.WaitForExit(); //We need to wait until this process completes and the CMD window closes
+            }
+        }
+
+        [TestMethod, TestCategory("Integration")]
+        public void ATestThatNeverRuns()
+        {
+            Assert.Fail("this test should not even run in AppVeyor right now");
+        }
+    }
+}
