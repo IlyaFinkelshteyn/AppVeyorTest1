380f7273e93e51989ab4993d083e0fa8cbf72471
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-08-17 21:38:59 -0700

    Update DoCommandLineStuff.bat

--- DoCommandLineStuff.bat
+++ DoCommandLineStuff.bat
@@ -29,10 +29,10 @@ IF not %errorlevel% equ 0 (
 )
 
 REM Setup the Visual Studio Build Environment
-call reg query HKLM\SOFTWARE\Microsoft\VisualStudio\SxS\VS7 /v 12.0
-IF %errorlevel% neq 0 GOTO :error
-REM CALL "%vs120comntools%\VsDevCmd.bat"
+REM call reg query HKLM\SOFTWARE\Microsoft\VisualStudio\SxS\VS7 /v 12.0
 REM IF %errorlevel% neq 0 GOTO :error
+CALL "%vs120comntools%\VsDevCmd.bat"
+IF %errorlevel% neq 0 GOTO :error
 
 ECHO:
 CALL SetColor.bat 0A "---- All Good it worked OK!"
